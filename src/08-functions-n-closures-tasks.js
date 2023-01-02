3e6c69af6ea98dca006a94de905470206a12718b
Margarita <Margarita>
2023-01-02 23:03:01 +0300

    feat: update 08-functions-n-closures-tasks decision

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -170,8 +170,14 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  const arr = [];
+  const func = (newStart = startFrom) => {
+    if (!arr.includes(newStart)) arr.push(newStart);
+    else return func(newStart + 1);
+    return newStart;
+  };
+  return func;
 }
 
 
